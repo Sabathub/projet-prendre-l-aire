2f01fa4433b9a17a4b4b514e1f632fac4ea930b0
Sabathub <maxjudu73@hotmail.fr>
2020-01-16 18:21:02 +0100

    modif menu & style navbar

--- front/src/components/Header/index.js
+++ front/src/components/Header/index.js
@@ -10,21 +10,15 @@ class Header extends Component {
     return (
       <>
 {/* // Composant Navbar et Nav de Semantic UI pour le menu de navigation */}
-        <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="#home" title="Retour à l'acceuil">
-            <img
-              alt=""
-              src="/src/images/logo.svg"
-              width="30"
-              height="30"
-              className="d-inline-block align-middle"
-            />{' '}
-            Prendre l'aire
+        <Navbar id="navbar" className="navbar-dark" expand="lg">
+          <Navbar.Brand className="font-italic" href="#home" title="Retour à l'acceuil">
+            <span className="logo"></span>
+            <div className="d-inline-block text-wrap align-middle text-left" id="title">&nbsp;Prendre l'aire</div>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav" >
             <Nav className="ml-auto">
-              <Nav.Link href="#home" title="Retour à l'acceuil">Accueil</Nav.Link>
+              <Nav.Link className="active" href="#home" title="Retour à l'acceuil">Accueil</Nav.Link>
               <Nav.Link href="#signin" title="Créer un compte">Inscription</Nav.Link>
               <Nav.Link href="#signup" title="Se connecter">Connexion</Nav.Link>
               <Nav.Link href="#profile" title="Accéder à mon profil">Profil</Nav.Link>
