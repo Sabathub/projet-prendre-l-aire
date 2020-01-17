2c1275b30b217f4da36ea9a96077fa71e81bd8e0
Sabathub <maxjudu73@hotmail.fr>
2020-01-17 18:47:24 +0100

    form OK

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -4,14 +4,16 @@ import React from 'react';
 // == Import : local
 import './app.scss';
 import Header from 'src/components/Header';
-import Home from 'src/components/Home';
+// import Home from 'src/components/Home';
+import Login from 'src/components/Login';
 import Footer from 'src/components/Footer';
 
 // == Composant
 const App = () => (
   <div id="app">
     <Header />
-    <Home />
+    {/* //<Home /> */}
+    <Login />
     <Footer />
   </div>
 );
