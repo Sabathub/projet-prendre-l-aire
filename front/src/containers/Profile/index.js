c6a87a8d24b104aac24afdd8d6bda5d246de3e55
Sabathub <maxjudu73@hotmail.fr>
2020-01-20 16:48:12 +0100

    Add Redux, React-Redux, Axios and Update files

---
+++ front/src/containers/Profile/index.js
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux';
+
+import Profile from 'src/components/Profile';
+
+
+const mapStateToProps = () => ({
+});
+
+const mapDispatchToProps = () => ({
+});
+
+const ProfileContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Profile);
+
+export default ProfileContainer;
---
+++ front/src/store/ajaxMiddleware.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+import { SWITCH_OFF } from './reducer';
+
+const ajaxMiddleware = (store) => (next) => (action) => {
+   switch (action.type) {
+    case SWITCH_OFF:
+      // appel axios
+      axios.get('https://raw.githubusercontent.com/raywenderlich/recipes/master/Recipes.json')
+        .then((response) => {
+          const switchOfAction = switchOf(response.data);
+          store.dispatch(switchOfAction);
+        })
+        .catch((error) => {
+          // console.error permet d'afficher une erreur dans la console
+          console.error(error);
+        })
+        .finally(() => {
+          // voir https://github.com/o-clock-wave/react-e16-challenge-solo-recipes-api-cecileb-oclock/blob/master/src/store/ajaxMiddleware.js
+          const stopLoadingAction = stopLoading();
+          store.dispatch(stopLoadingAction);
+        });
+      break;
+
+    default:
+      // je laisse passer les autres actions
+      next(action);
+  }
+};
+
+export default ajaxMiddleware;
