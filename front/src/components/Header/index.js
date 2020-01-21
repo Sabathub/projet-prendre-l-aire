b2f84c4f490fce11fe479231335a2c7e26d1c384
Sabathub <maxjudu73@hotmail.fr>
2020-01-21 11:17:21 +0100

    class header -> const header

--- front/src/components/Header/index.js
+++ front/src/components/Header/index.js
@@ -1,29 +1,33 @@
 // Import NPM
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
-import Logo from './../../assets/logo_contour.svg'
+import Logo from '../../assets/logo_contour.svg';
 
 // Import local
 import './header.scss';
 
-class Header extends Component {
-  render() {
+const Header = () =>  {
     return (
       <>
-{/* // Composant Navbar et Nav de Boostrap pour le menu de navigation */}
+        {/* // Composant Navbar et Nav de Boostrap pour le menu de navigation */}
         <Navbar id="navbar" className="navbar-dark" expand="lg">
-          <Navbar.Brand className="font-italic" href="#home" title="Retour à l'acceuil">
-            <img
-              alt=""
-              src={Logo}
-              height="80"
-              className="d-inline-block align-middle"
-            />{' '}
-            <div className="d-inline-block text-wrap align-middle text-left" id="title">&nbsp;Prendre l'aire</div>
+          <Navbar.Brand className="font-italic" title="Retour à l'acceuil">
+            <Link
+              to="/"
+              exact
+            >
+              <img
+                alt=""
+                src={Logo}
+                height="80"
+                className="d-inline-block align-middle"
+              />{' '}
+              <div className="d-inline-block text-wrap align-middle text-left" id="title">&nbsp;Prendre l'aire</div>
+            </Link>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" >
+          <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
               <NavLink
                 to="/"
@@ -66,10 +70,6 @@ class Header extends Component {
         </Navbar>
       </>
     );
-  }
 }
 
- export default Header;
-
-
-
+export default Header;
