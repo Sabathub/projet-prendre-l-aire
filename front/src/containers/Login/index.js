a2ce473c6878a304bdfea4f6b1aeb97cd8cd9f39
Sabathub <maxjudu73@hotmail.fr>
2020-01-23 11:18:36 +0100

    props on container login done!

--- front/src/containers/Login/index.js
+++ front/src/containers/Login/index.js
@@ -2,11 +2,21 @@ import { connect } from 'react-redux';
 
 import Login from 'src/components/Login';
 
+import { changeInput, doConnect } from 'src/store/actions';
 
-const mapStateToProps = () => ({
+
+const mapStateToProps = (state) => ({
+  emailValue: state.form.email,
+  passwordValue: state.form.password,
 });
 
-const mapDispatchToProps = () => ({
+const mapDispatchToProps = (dispatch) => ({
+  changeInputValue: (value, name) => {
+    dispatch(changeInput(value, name));
+  },
+  doSignup: () => {
+    dispatch(doConnect());
+  },
 });
 
 const LoginContainer = connect(
