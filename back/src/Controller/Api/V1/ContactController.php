f81e14794e69d2f829df2aa661de1977c3ba29e7
MarionPolomeni <marion.9595@hotmail.fr>
2020-01-28 09:07:48 +0100

    reset registercontroller with original version

--- back/src/Controller/Api/V1/ContactController.php
+++ back/src/Controller/Api/V1/ContactController.php
@@ -1,53 +1,63 @@
+  
 <?php
 
-namespace App\Controller\Api\v1;
+namespace App\Controller\Api\V1;
 
-use App\Entity\Contact;
-use App\Form\ContactType;
+use App\Entity\User;
+use App\Form\UserType;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-class ContactController extends AbstractController
+class RegisterController extends AbstractController
 {
     /**
-     * @Route("api/v1/contact", name="api_v1_contact", methods={"POST"})
+     * Create a new user
+     * @Route("/api/v1/register", name="api_v1_register", methods={"POST"})
      */
-    public function contact(Request $request, \Swift_Mailer $mailer)
+    public function register(Request $request, SerializerInterface $serializer, UserPasswordEncoderInterface $passwordEncoder): Response
     {
         // first action we get the Json content
-        $newContact = $request->getContent();
+        $newUser = $request->getContent();
 
         // then decode Json
-        $data = json_decode($newContact, true);
+        $data = json_decode($newUser, true);
 
         // and replace this into the request with parameters in array shape
         $request->request->replace(is_array($data) ? $data : array());
 
-        // second action is to create a new object contact and a ContactType form associated that we fill with the request
-        $contact = new Contact();
-        $form = $this->createForm(ContactType::class, $contact, ['csrf_protection' => false]);
+        // second action is to create a new object user and a UserType form associated that we fill with the request
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user, ['csrf_protection' => false]);
         $form->handleRequest($request);
-     
 
         if ($form->isSubmitted() && $form->isValid()) {
 
-            // Let's set the message with form data
-            $message = (new \Swift_Message('Re : '.$form['subject']->getData()))
-                ->setFrom($form['email']->getData())
-                ->setTo('contact.prendrelaire@gmail.com')
-                ->setReplyTo($form['email']->getData())
-                ->setBody(
-                    $form['content']->getData(),
-                    'text/html'
-            );
+            // attribute role user
+            $user->setRoles(['ROLE_USER']);
 
-            // send message
-            $mailer->send($message);
+            // get plain password 
+            $plainPassword = $user->getPassword();
 
-            return $this->json('mail envoyé');
+            // encode password
+            $encodedPassword = $passwordEncoder->encodePassword($user, $plainPassword);
+
+            // replace plain password by the hashed password
+            $user->setPassword($encodedPassword);
+
+            // save in database
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // return JSON
+            return $this->json('user registered');
         }
-        
-        return $this->json('failed to send the message');
+
+        // return JSON if not a success
+        return $this->json('failed to register');
     }
 }
