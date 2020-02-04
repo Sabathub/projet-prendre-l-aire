408a6f457cc1641ae2e49ecaa90963a1778aa57a
Sabathub <maxjudu73@hotmail.fr>
2020-02-04 15:06:18 +0100

    add css

--- front/src/components/Legalmentions/index.js
+++ front/src/components/Legalmentions/index.js
@@ -8,9 +8,46 @@ import './legalmentions.scss';
 
 // == Composant
 const Legalmentions = () => (
-  <div>
-    <h1>Mentions Légales</h1>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non velit ipsa laudantium amet adipisci cumque maxime soluta quaerat quas pariatur, accusamus dolore fugiat debitis. Earum officia, nulla magni quisquam doloremque inventore vero ea ut accusantium nemo explicabo perspiciatis voluptas cupiditate deleniti aliquam repellendus quas tempora consequuntur corrupti iusto perferendis odit numquam at architecto? A, ut? Molestias officia quo laudantium reprehenderit voluptatum deleniti, nisi veritatis totam amet soluta eveniet nostrum commodi vel quasi excepturi saepe facere, doloremque nam. Explicabo consequatur vel omnis reiciendis alias qui inventore odit assumenda. Nisi exercitationem consequatur dolores modi ut iure quae autem sed illum repellendus? Qui nobis iure voluptas quis ipsa. Molestiae vero accusantium eaque, officia debitis magnam a optio dolores aliquid neque! Ad at molestiae, tempora minima, quaerat expedita eos, quis quam suscipit debitis rem inventore. Vel minima aperiam incidunt voluptatum. Quisquam voluptatibus nam, error sit, quas laudantium soluta facilis nesciunt possimus cumque hic amet officia est? Magnam iure rem aliquid maiores, officia quasi fugit consequatur animi voluptatibus labore neque natus ducimus cumque dolorum perferendis repudiandae similique. Voluptatum, tempora! Commodi accusamus at repellat cum minus harum inventore assumenda, dolore, quo, consectetur voluptatum ut! Quae magnam perferendis minima molestias, in iste odit, sapiente at illo deleniti, blanditiis aliquam praesentium autem? Sint natus fuga amet atque tenetur doloribus cumque sunt, ut minus impedit quidem officiis a repellat alias veniam molestias! Et, nisi id voluptate ipsam ullam repellendus dolores labore asperiores eius! Maiores, corrupti magnam repudiandae explicabo earum ducimus veritatis quas optio voluptates quos exercitationem quis odio consequatur quod hic facilis. Velit assumenda minus, quas esse suscipit reprehenderit fuga inventore consequuntur deserunt repellendus quod, sed ex quis aut sapiente debitis accusamus molestiae modi. Voluptatum beatae quia culpa aperiam suscipit a eum soluta necessitatibus recusandae esse, assumenda voluptas laboriosam deleniti eveniet ipsa libero hic nesciunt veritatis incidunt quas vitae.</p>
+  <div id="legalMentionsContainer">
+    <h1 id="legalMentionsTitle">Mentions Légales</h1>
+    <p className="legalMentions">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Non velit ipsa laudantium amet adipisci cumque maxime soluta quaerat quas pariatur,
+        accusamus dolore fugiat debitis. Earum officia,
+        nulla magni quisquam doloremque inventore vero ea ut
+        accusantium nemo explicabo perspiciatis
+        voluptas cupiditate deleniti aliquam repellendus quas tempora consequuntur corrupti
+        iusto perferendis odit numquam at architecto? A, ut? Molestias officia quo
+        laudantium reprehenderit voluptatum deleniti, nisi veritatis totam amet soluta eveniet
+        nostrum commodi vel quasi excepturi saepe facere, doloremque nam.
+    </p>
+    <p className="legalMentions">
+        Explicabo consequatur vel omnis reiciendis alias qui inventore odit assumenda.
+        Nisi exercitationem consequatur dolores modi ut iure quae autem sed illum repellendus?
+        Qui nobis iure voluptas quis ipsa. Molestiae vero accusantium eaque,
+        officia debitis magnam a optio dolores aliquid neque! Ad at molestiae,
+        tempora minima, quaerat expedita eos, quis quam suscipit debitis rem inventore.
+        Vel minima aperiam incidunt voluptatum. Quisquam voluptatibus nam,
+        error sit, *quas laudantium soluta facilis nesciunt possimus
+        cumque hic amet officia est? Magnam iure rem aliquid maiores,
+        officia quasi fugit consequatur animi voluptatibus labore neque natus ducimus
+        cumque dolorum perferendis repudiandae similique. Voluptatum, tempora!
+        Commodi accusamus at repellat cum minus harum inventore assumenda, dolore, quo,
+        consectetur voluptatum ut! Quae magnam perferendis minima molestias, in iste odit,
+        sapiente at illo deleniti, blanditiis aliquam praesentium autem? Sint natus fuga
+        amet atque tenetur doloribus cumque sunt, ut minus impedit quidem
+        officiis a repellat alias veniam molestias!
+    </p>
+    <p className="legalMentions">
+        Et, nisi id voluptate ipsam ullam
+        repellendus dolores labore asperiores eius! Maiores, corrupti magnam
+        repudiandae explicabo earum ducimus veritatis quas optio voluptates quos
+        exercitationem quis odio consequatur quod hic facilis.
+        Velit assumenda minus, quas esse suscipit reprehenderit fuga inventore consequuntur
+        deserunt repellendus quod, sed ex quis aut sapiente debitis accusamus molestiae modi.
+        Voluptatum beatae quia culpa aperiam suscipit a eum soluta necessitatibus recusandae esse,
+        assumenda voluptas laboriosam deleniti eveniet ipsa libero
+        hic nesciunt veritatis incidunt quas vitae.
+    </p>
   </div>
 );
 
